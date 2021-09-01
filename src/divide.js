0e2fde47c743ee3f7fc7ed685c7f078ba0a199d7
QWERTYCODE <contact@tram24sentia.com>
2021-08-31 23:16:44 -0400

    ➖✅

--- src/divide.js
+++ src/divide.js
@@ -1,40 +1,46 @@
 const prompts = require("prompts");
 const askFinish = require("./askFinish");
+const chalk = require("chalk");
 
 const divide = async () => {
-  console.clear();
-  console.log("");
+    console.clear();
+    console.log("");
 
-  const divideAk = [
-    {
-      type: "number",
-      name: "di",
-      message: "[/] Enter a Number to Divide> ",
-      style: "default",
-      initial: "46"
-    },
-  ];
+    const divideAk = [
+        {
+            type: "number",
+            name: "di",
+            message: "[/] Enter a Number to Divide> ",
+            style: "default",
+            initial: "46"
+        },
+    ];
 
-  let diK = await prompts(divideAk);
+    let diK = await prompts(divideAk);
 
-  console.log("");
+    console.log("");
 
-  const diAkT = [
-    {
-      type: "number",
-      name: "dv",
-      message: "[/] Enter a Number to Divide> ",
-      style: "default",
-      initial: "47"
-    },
-  ];
+    const diAkT = [
+        {
+            type: "number",
+            name: "dv",
+            message: "[/] Enter a Number to Divide> ",
+            style: "default",
+            initial: "47"
+        },
+    ];
 
-  let diKT = await prompts(diAkT);
+    let diKT = await prompts(diAkT);
 
-  console.log("");
-  console.log(" [=] result:", diK.di / diKT.dv);
+    console.log("");
 
-  askFinish();
+    if (diKT.dv == 0) {
+        console.log(chalk.red("An error has occurred"));
+    } else {
+        console.log(" [=] result:", diK.di / diKT.dv);
+    }
+
+    askFinish();
 };
 
 module.exports = divide;
